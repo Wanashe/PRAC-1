1eac810b02950003794b24dfdaedd11e40a4bf61
Tadiwanashe Mbendana <mbendanatadiwanashe@gmail.com>
2026-09-17 10:27:12 +0200

    dtabase and forms

--- firstapp/models.py
+++ firstapp/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 # Create your models here.
 class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.IntegerField(max_digits=5, decimal_places=2)
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
     description = models.TextField()
 
     def __str__(self):
         return self.name
+
+class Reservation(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    guest_count = models.IntegerField()
+    reservation_time = models.DateField(auto_now=True)
+    comments = models.CharField(max_length=1000)
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import ReservationForm
 
 # Create your views here.
 def hello_world(request):
@@ -10,3 +11,14 @@ def hello_world(request):
 class HelloEthopia(View):
     def get(self, request):
         return HttpResponse("hello Ethopia")
+
+def home(request):
+    form = ReservationForm() 
+
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save() 
+            return HttpResponse("Reservation successful!")
+
+    return render(request, 'index.html', {'form':  form})
